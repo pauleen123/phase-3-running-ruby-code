6f9251d0a2bfd8dad727a982676179133f4a593e
+ <+>
2022-10-17 10:33:36 +0300

    lab1

---
+++ app.rb
@@ -0,0 +1,9 @@
+puts 'Hello World!'
+
+p [1,2,3]
+
+p [{id: 1, name: 'pauline'}, {id: 2,name: 'mercy'}]
+
+# pp [{ id: 1, name: "World" }, { id: 2, name: "Ruby" }, { id: 3, name: "Moon" }, { id: 4, name: "Learner" }]
+
+print 'Pass this test, please.'
